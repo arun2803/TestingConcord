c7cb3003e4f5201a7ac29304f073ab58fa2351cb
arun2803 <a.kumar2803@gmail.com>
2020-09-17 13:44:52 +0530

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,8 +1,9 @@
-def test(ctx, x, a, b, c, d):
+def test(ctx, x, a, b, c, d, e):
     print 'Hello from another module!'
     print x
     print a
     print b
     print c
     print d
+    print e
     ctx.setVariable('y', x + 3)
